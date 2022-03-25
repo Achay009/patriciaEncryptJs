29a269a18e19c41d7576475f37fbe3f6cdcae09c
AchayInc <uche.echesurum@mypatricia.co>
2022-03-25 21:36:07 +0100

    hopefully it runs i guess

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ patcrypt.encrypt = function(){
 
 }
 
-patcrypt
+patcrypt.encryptFile = function(filePath, publicKeyPath){
+    const publicKey = fs.readFileSync(publicKeyPath)
+    const file = fs.readFileSync(filePath)
+    const encrypted = crypto.publicEncrypt(publicKey, file)
+    const encryptedFilePath = path.join(path.dirname(filePath), path.basename(filePath, path.extname(filePath)) + '.pat')
+    fs.writeFileSync(encryptedFilePath, encrypted)
+}
+
+
+patcrypt.decryptFile = function(filePath, privateKeyPath){
+    const privateKey = fs.readFileSync(privateKeyPath)
+    const file = fs.readFileSync(filePath)
+    const decrypted = crypto.privateDecrypt(privateKey, file)
+    const decryptedFilePath = path.join(path.dirname(filePath), path.basename(filePath, path.extname(filePath)) + '.pat')
+    fs.writeFileSync(decryptedFilePath, decrypted)
+}
 
 patcrypt.generateKeyPair = function(){
     const {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
